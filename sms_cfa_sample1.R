2e20780db4addd24bfefabd3d5cac5966b8a6670
PPALab <ppalab@ad.wsu.edu>
2020-02-03 12:29:56 -0800

    asad

--- sms_cfa_sample1.R
+++ sms_cfa_sample1.R
@@ -1,5 +1,5 @@
 library(haven)
-sms_sample1 <- read_spss('S:/shared/PPALab/SMS PA 2/Data/SMSPA2_surveydata_jan2020sample1_11.sav')
+sms_sample1 <- read_spss('S:/shared/PPALab/SMS PA 2/Data/SMSPA2_surveydata_jan2020sample1_147.sav')
 
 
 # Age descriptive stats
@@ -42,9 +42,18 @@ is.na(sms_sample1) <- sms_sample1 == 88
 is.na(sms_sample1) <- sms_sample1 > 4
 sum(is.na(sms_sample1))
 
-library(BaylorEdPsych)
-library(mvnmle)
-LittleMCAR(sms_sample1)
+# Count rows with missing values
+row.has.na <- apply(sms_sample1, 1, function(x){any(is.na(x))})
+sum(row.has.na)
+
+# Percent missing values by variable
+percentmiss <- function(x){
+  sum(is.na(x)) / length(x) * 100} 
+
+apply(sms_sample1, 2, percentmiss)
+
+# Number of subjects with missing values by percent
+table(apply(sms_sample1, 1, percentmiss))
 
 
 
@@ -61,6 +70,40 @@ AcceptB =~ AcceptB_3 + AcceptB_4 + AcceptB_5 + AcceptB_6'
 fit <- cfa(model, data = sms_sample1, estimator = "WLSMVS")
 
 summary(fit, fit.measures = TRUE, rsq = TRUE, standardized = TRUE)
+
+
+
+bf.model2 <-
+  'gen.factor =~ SMindM_1 + SMindM_3 + SMindM_4 + SMindM_5 + 
+SMindB_3 + SMindB_4 + SMindB_5 + SMindB_6 + 
+AcceptM_3 + AcceptM_4 + AcceptM_6 +
+AcceptB_3 + AcceptB_4 + AcceptB_5 + AcceptB_6
+body.factor =~ SMindB_3 + SMindB_4 + SMindB_5 + SMindB_6 + 
+AcceptB_3 + AcceptB_4 + AcceptB_5 + AcceptB_6
+mind.factor =~ SMindM_1 + SMindM_3 + SMindM_4 + SMindM_5 + 
+AcceptM_3 + AcceptM_4 + AcceptM_6'
+
+
+bf.fit2 <- cfa(bf.model, data = sms_sample1, orthogonal = T)
+
+summary(bf.fit, fit.measures = T, rsq = T, standardized = T)
+
+
+bf.model4 <-
+  'gen.factor =~ SMindM_1 + SMindM_3 + SMindM_4 + SMindM_5 + 
+SMindB_3 + SMindB_4 + SMindB_5 + SMindB_6 + 
+AcceptM_3 + AcceptM_4 + AcceptM_6 +
+AcceptB_3 + AcceptB_4 + AcceptB_5 + AcceptB_6
+SMindM =~ SMindM_1 + SMindM_3 + SMindM_4 + SMindM_5
+SMindB =~  SMindB_3 + SMindB_4 + SMindB_5 + SMindB_6
+AcceptM =~ AcceptM_3 + AcceptM_4 + AcceptM_6
+AcceptB =~ AcceptB_3 + AcceptB_4 + AcceptB_5 + AcceptB_6'
+
+bf.fit4 <- cfa(bf.model4, data = sms_sample1, estimator = "WLSMVS")
+
+summary(bf.fit4, fit.measures = T, rsq = T, standardized = T)
+
+
 
 
 
--- sms_efa_sample0.R
+++ sms_efa_sample0.R
@@ -5,9 +5,11 @@
 
 # Import sav file as data frame
 library(haven)
-sms_sample0 <- read_spss('S:/shared/PPALab/SMS PA 2/Data/SMSPA2_surveydata_jan2020sample0_11.sav')
+sms_sample0 <- read_spss('S:/shared/PPALab/SMS PA 2/Data/SMSPA2_surveydata_jan2020sample0_139.sav')
 
-
+library(psych)
+library(dplyr)
+library(GPArotation)
 # Age descriptive stats
 summary(sms_sample0$AGE)
 sd(sms_sample0$AGE, na.rm = TRUE) #standard deviation
@@ -43,7 +45,6 @@ sum(sms_sample0$CLASS == "UREC") / nrow(sms_sample0) * 100
 
 
 # Create new data frame with columns from SMindM_1 to AcceptB_13 
-library(dplyr)
 sms_sample0 <- sms_sample0 %>% 
   select(SMindM_1:AcceptB_13)
 
@@ -55,7 +56,7 @@ sum(is.na(sms_sample0))
 
 library(BaylorEdPsych)
 library(mvnmle)
-LittleMCAR(sms_sample1)
+LittleMCAR(sms_sample0)
 
 # Name data frame 'efadat'
 efadat <- sms_sample0
@@ -79,8 +80,7 @@ efadat <- efadat[-c(2), ]
 
 
 # Correlation Matrix
-library(psych)
-correlation <- cor(efadat, use = "complete.obs") #casewise deletion
+correlation <- cor(efadat, use = "pairwise.complete.obs") #pairwise deletion
 
 # Correlogram w/significance using corrplot
 library(corrplot)
@@ -134,58 +134,58 @@ parallel <-
 
 # https://sakaluk.wordpress.com/2016/05/26/11-make-it-pretty-scree-plots-and-parallel-analysis-using-psych-and-ggplot2/
 
-# obs = data.frame(parallel$fa.values)
-# obs$type = c('Observed Data')
-# obs$num = c(row.names(obs))
-# obs$num = as.numeric(obs$num)
-# colnames(obs) = c('eigenvalue', 'type', 'num')
-# 
-# View(obs)
-# #Calculate quantiles for eigenvalues, but only store those from simulated CF model in percentile1
-# percentile = apply(parallel$values,2,function(x) quantile(x,.95))
-# min = as.numeric(nrow(obs))
-# min = (4*min) - (min-1)
-# max = as.numeric(nrow(obs))
-# max = 4*max
-# percentile1 = percentile[min:max]
-# 
-# # Create data frame called sim; with simulated eigenvalue data
-# sim = data.frame(percentile1)
-# sim$type = c('Simulated Data (95th %ile)')
-# sim$num = c(row.names(obs))
-# sim$num = as.numeric(sim$num)
-# colnames(sim) = c('eigenvalue', 'type', 'num')
-# 
-# #Merge the two data frames (obs and sim) together into data frame called eigendat
-# eigendat = rbind(obs,sim)
-# library(ggplot2)
-# apatheme=theme_bw()+
-#   theme(panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank(),
-#         panel.background = element_blank(),
-#         panel.border = element_blank(),
-#         text=element_text(family='Arial'),
-#         legend.title=element_blank(),
-#         legend.position=c(.7,.8),
-#         axis.line.x = element_line(color='black'),
-#         axis.line.y = element_line(color='black'))
-# 
-# #Use data from eigendat. Map number of factors to x-axis, eigenvalue to y-axis, and give different data point shapes depending on whether eigenvalue is observed or simulated
-# p = ggplot(eigendat, aes(x=num, y=eigenvalue, shape=type)) +
-#   #Add lines connecting data points
-#   geom_line()+
-#   #Add the data points.
-#   geom_point(size=4)+
-#   #Label the y-axis 'Eigenvalue'
-#   scale_y_continuous(name='Eigenvalue')+
-#   #Label the x-axis 'Factor Number', and ensure that it ranges from 1-max # of factors, increasing by one with each 'tick' mark.
-#   scale_x_continuous(name='Factor Number', breaks=min(eigendat$num):max(eigendat$num))+
-#   #Manually specify the different shapes to use for actual and simulated data, in this case, white and black circles.
-#   scale_shape_manual(values=c(16,1)) +
-#   #Add vertical line indicating parallel analysis suggested max # of factors to retain
-#   geom_vline(xintercept = parallel$nfact, linetype = 'dashed')+
-#   #Apply our apa-formatting theme
-#   apatheme
+obs = data.frame(parallel$fa.values)
+obs$type = c('Observed Data')
+obs$num = c(row.names(obs))
+obs$num = as.numeric(obs$num)
+colnames(obs) = c('eigenvalue', 'type', 'num')
+
+View(obs)
+#Calculate quantiles for eigenvalues, but only store those from simulated CF model in percentile1
+percentile = apply(parallel$values,2,function(x) quantile(x,.95))
+min = as.numeric(nrow(obs))
+min = (4*min) - (min-1)
+max = as.numeric(nrow(obs))
+max = 4*max
+percentile1 = percentile[min:max]
+
+# Create data frame called sim; with simulated eigenvalue data
+sim = data.frame(percentile1)
+sim$type = c('Simulated Data (95th %ile)')
+sim$num = c(row.names(obs))
+sim$num = as.numeric(sim$num)
+colnames(sim) = c('eigenvalue', 'type', 'num')
+
+#Merge the two data frames (obs and sim) together into data frame called eigendat
+eigendat = rbind(obs,sim)
+library(ggplot2)
+apatheme=theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        text=element_text(family='Arial'),
+        legend.title=element_blank(),
+        legend.position=c(.7,.8),
+        axis.line.x = element_line(color='black'),
+        axis.line.y = element_line(color='black'))
+
+#Use data from eigendat. Map number of factors to x-axis, eigenvalue to y-axis, and give different data point shapes depending on whether eigenvalue is observed or simulated
+p = ggplot(eigendat, aes(x=num, y=eigenvalue, shape=type)) +
+  #Add lines connecting data points
+  geom_line()+
+  #Add the data points.
+  geom_point(size=4)+
+  #Label the y-axis 'Eigenvalue'
+  scale_y_continuous(name='Eigenvalue')+
+  #Label the x-axis 'Factor Number', and ensure that it ranges from 1-max # of factors, increasing by one with each 'tick' mark.
+  scale_x_continuous(name='Factor Number', breaks=min(eigendat$num):max(eigendat$num))+
+  #Manually specify the different shapes to use for actual and simulated data, in this case, white and black circles.
+  scale_shape_manual(values=c(16,1)) +
+  #Add vertical line indicating parallel analysis suggested max # of factors to retain
+  geom_vline(xintercept = parallel$nfact, linetype = 'dashed')+
+  #Apply our apa-formatting theme
+  apatheme
 # #Call the plot. Looks pretty!
 # p
 # # ggsave('parallel.png', width=6, height=6, unit='in', dpi=300)
@@ -275,7 +275,8 @@ alpha(efadat[, AcceptB])
 
 #exploratory bifactor
 
-
+#update samples
+#import to xl
 
 
 
